c08ff079e930903347bf2ca8cb105adf8352eee1
Nicolas Chambrier <naholyr@gmail.com>
2016-09-20 14:01:17 +0200

    todos:add

--- src/AddForm.js
+++ src/AddForm.js
@@ -2,22 +2,63 @@ import React, { Component } from 'react';
 import dispatcher from './my-flux/dispatcher';
 
 
+// Controlled Input
+
 class AddForm extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = { text: '' };
+  }
 
   onClick (e) {
     e.preventDefault();
-    dispatcher.emit('todos:add', 'Nouveau truc à faire');
+    dispatcher.emit('todos:add', this.state.text);
+    this.setState({ text: '' });
+  }
+
+  updateText (e) {
+    this.setState({ text: e.target.value });
   }
 
   render() {
     return (
       <form>
-        <input type="text" />
+        <input type="text" value={ this.state.text } onChange={ (e) => this.updateText(e) } />
         <button type="submit" onClick={ (e) => this.onClick(e) }>Add</button>
       </form>
     );
   }
 
 }
+
+
+/* Uncontrolled Input
+
+class AddForm extends Component {
+
+  onClick (e) {
+    e.preventDefault();
+    dispatcher.emit('todos:add', this._currentText);
+    this._currentText = '' /// Oui mais là ça render pas :(
+  }
+
+  updateText (e) {
+    this._currentText = e.target.value;
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" defaultValue="Task description" onChange={ (e) => this.updateText(e) } />
+        <button type="submit" onClick={ (e) => this.onClick(e) }>Add</button>
+      </form>
+    );
+  }
+
+}
+*/
+
 
 export default AddForm;
