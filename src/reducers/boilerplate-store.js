5ff514d3822e94a3d790560c4dec51d7ea56180b
Nicolas Chambrier <naholyr@gmail.com>
2016-09-21 10:43:03 +0200

    boilerplate for new reducers module

---
+++ src/reducers/boilerplate-store.js
@@ -0,0 +1,15 @@
+import { ??? } from '???';
+import { createReducer } from '../helpers';
+
+
+const initialState = ???
+
+const reducers = {
+
+  [???]: (prevState, payload) =>
+    newState,
+
+}
+
+
+export default createReducer(initialState, reducers);
